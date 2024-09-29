c7bc4c144a32d8f2f6d87db8197eee0f4a84a368
Bharat Bhushan <bharat.bhushan@planetspark.in>
2024-09-29 19:53:51 +0530

    update

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,5 +1,5 @@
 class TelegramWebhooksController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   require 'telegram/bot'
 
   def receive

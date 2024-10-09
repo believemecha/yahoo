37b7c563397795713f021a366a4b7fcb59e5eba4
Bharat Bhushan <bharat.bhushan@planetspark.in>
2024-10-09 19:45:36 +0530

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,16 +74,9 @@ Rails.application.configure do
   #for devise mailings
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'lbsingh732196@gmail.com', reply_to: 'lbsingh732196@gmail.com'}
+  config.action_mailer.default_options = {from: 'abc@gmail.com', reply_to: 'abc@gmail.com'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            'lbsingh732196@gmail.com',
-  password:             'gzzw eabf bilj dths',
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+
   config.action_mailer.default_url_options = {:host =>'localhost:3001'}
 
 
